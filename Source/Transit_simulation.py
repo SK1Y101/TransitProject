1d130790ad84f83d35ce39fabc9f4f59e7f14b32
sk1y101 <skiy@outlook.com>
2022-03-10 21:20:48 +0000

    better n-body solver

---
+++ Source/Transit_simulation.py
@@ -0,0 +1,129 @@
+import rebound
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pylab as plt
+from scipy.fft import rfft, rfftfreq, irfft
+from scipy.signal import find_peaks
+import scipy as sp
+
+from matplotlib.lines import Line2D
+
+sim = rebound.Simulation()
+# HAT-P-13
+sim.add(m=1.261)
+# HAT-P-13 b
+sim.add(m=1e-5 * 0.851, P=2.91626, e=0.021, omega=181, inc=np.radians(90-83.4))
+# HAT-P-13 b
+sim.add(m=1e-5 * 14.28, P=445.81, e=0.6616, omega=176.7)
+# move to the centre of mass
+sim.move_to_com()
+
+# step forward by one thousandth of an orbit
+tstep = 2.91626 / 360
+
+# ensure we collect 1000 transits
+N=520
+transittimes = np.zeros(N)
+p = sim.particles
+i = 0
+
+def gen():
+    while i<N:
+        yield
+
+for _ in tqdm(gen()):
+    y_old = p[1].y - p[0].y  # (Thanks to David Martin for pointing out a bug in this line!)
+    t_old = sim.t
+    sim.integrate(sim.t+tstep) # check for transits every 0.5 time units. Note that 0.5 is shorter than one orbit
+    t_new = sim.t
+    if y_old*(p[1].y-p[0].y)<0. and p[1].x-p[0].x>0.:   # sign changed (y_old*y<0), planet in front of star (x>0)
+        while t_new-t_old>1e-7:   # bisect until prec of 1e-5 reached
+            if y_old*(p[1].y-p[0].y)<0.:
+                t_new = sim.t
+            else:
+                t_old = sim.t
+            sim.integrate( (t_new+t_old)/2.)
+        transittimes[i] = sim.t
+        i += 1
+        sim.integrate(sim.t+tstep)       # integrate 0.05 to be past the transit
+
+# plot the position of bodies
+fig, ax_main, ax_top, ax_right = rebound.OrbitPlot(sim, slices=0.5, xlim=[-2.,2], ylim=[-2.,2], color=True, unitlabel="[AU]")
+fig.suptitle("HAT-P-13")
+
+legend_elements = [Line2D([0], [0], color=(1.,0.,0.), lw=1.5, label='HAT-P-13b Orbit'),
+                   Line2D([0], [0], color=(0.,0.75,0.75), lw=1.5, label='HAT-P-13c Orbit'),
+                   Line2D([0], [0], marker="o", markersize=10, label='HAT-P-13b', markerfacecolor="black", c="White"),
+                   Line2D([0], [0], marker="*", markersize=15, label="HAT-P-13", markerfacecolor="black", c="White")]
+
+ax_main.legend(handles = legend_elements, loc="best")
+fig.tight_layout()
+fig.savefig("HAT-P-13 layout.png")
+fig.show()
+
+print(sim.cite())
+
+# recover transit timing variation from linear regression
+A = np.vstack([np.ones(N), range(N)]).T
+c, m = np.linalg.lstsq(A, transittimes, rcond=-1)[0]
+
+TTV = (transittimes-m*np.array(range(N))-c)*(24.*365./2./np.pi)
+
+#fetch the limit from the maximum TTV
+lim = max(abs(TTV))
+
+# plot the variation
+fig = plt.figure(figsize=(10,5))
+ax = plt.subplot()
+plt.scatter(range(N), TTV, marker="x", c="red", s=15, label="Observed variation from Least Squares Regression")
+
+ax.set_xlim([-10,N-10])
+ax.set_ylim([-lim*1.1, lim*1.1])
+ax.set_xlabel("Transit number")
+ax.set_ylabel("TTV [hours]")
+ax.tick_params(axis="x",direction="in", pad=-15)
+ax.xaxis.labelpad = -20
+
+ax2 = ax.twiny()
+ax2.set_xlabel("Time [days]")
+ax2.xaxis.set_label_position("bottom")
+ax2.xaxis.tick_bottom()
+ax2.xaxis.labelpad = 0
+ax2.set_xlim([-10 * 2.91626, sim.t - 10*2.91626])
+
+ax.legend(loc="best")
+
+fig.suptitle("Simulated Transit Timing Variation in the HAT-P-13 System", fontsize="x-large")
+fig.tight_layout()
+plt.legend()
+plt.savefig("Simulated TTV")
+plt.show()
+
+# fourier analysis
+duration = transittimes[-1] - transittimes[0]
+sampling_rate = N / duration
+
+from scipy.fft import rfft, rfftfreq, irfft
+# compute the fourier transform of the residuals
+yf = rfft(TTV)
+xf = rfftfreq(N, 1 / sampling_rate)
+peaks = find_peaks(np.abs(yf), distance=10)[0]
+
+print(peaks)
+fig = plt.figure(figsize=(10,5))
+ax = plt.subplot()
+ax.plot(xf, np.abs(yf))
+plt.show()
+
+
+fig = plt.figure(figsize=(10,5))
+ax = plt.subplot()
+# remove the largest frequency
+plt.scatter(range(N), TTV, marker="x")
+plt.plot(irfft(yf), label="without {}th freq".format(i))
+ax.set_xlim([0,N])
+ax.set_xlabel("Transit number")
+ax.set_ylabel("TTV [hours]")
+ax.set_ylim([-lim, lim])
+
+plt.show()
--- Source/multi-planetary_transit_initial_test_script.py
+++ Source/multi-planetary_transit_initial_test_script.py
@@ -3,7 +3,7 @@ from astropy import constants as const
 from astropy import units as u
 import matplotlib.pylab as plt
 import matplotlib.animation as animation
-from scipy.fft import rfft, rfftfreq, irfft
+from scipy.fft import rfft, rfftfreq, irfft, next_fast_len
 from scipy.optimize import curve_fit
 from tqdm import trange, tqdm
 import numpy as np
@@ -29,14 +29,14 @@ def main():
 
     # add an earth-like planet at 0.02 AU
     primary_sma = 0.02*const.au
-    p1 = nbody.Body(mass = 5.9722E24*u.kg, radius=6371*u.km, colour = "blue", name = "Planet 1")
-    p1.keplerian(parent = star, sma = primary_sma, ecc = 0, inc = 0)
+    p1 = nbody.Body(mass = 1.98855E30*u.kg * 1e-5, radius=6371*u.km, colour = "blue", name = "Planet 1")
+    p1.keplerian(parent = star, sma = const.au, ecc = 0.1, inc = 0)
     system.addBody(p1)
 
     # add a super earth-like planet in 1:2 resonance
     resonance = 1/2
-    p2 = nbody.Body(mass = 4 * 5.9722E24*u.kg, radius=6371*u.km * 4**(1/3), colour = "green", name = "Planet 2")
-    p2.keplerian(parent = star, sma = primary_sma * resonance**(3/2), ecc = 0, inc = 0, arg=0)
+    p2 = nbody.Body(mass = 1.98855E30*u.kg * 1e-5, radius=6371*u.km * 4**(1/3), colour = "green", name = "Planet 2")
+    p2.keplerian(parent = star, sma = 1.757 * const.au, ecc = 0, inc = 0, arg=0)
 
     # set the system timestep
     system.tstep = int(min(p1.period, p2.period) / 200)
@@ -102,48 +102,44 @@ def main():
         return np.array([float(t) for t in np.array(transit)])
 
     # number of steps to integrate
-    integration_steps = 100 * 200
-    # ensure it's a power of two for fourier fitting
-    integration_steps = int(2 ** np.ceil(np.log(integration_steps) / np.log(2)))
+    integration_steps = 200 * 200
+    # move up to the next fastest length for fourier fitting
+    integration_steps = next_fast_len(integration_steps, real=True)
     # compute transits for the system with just the star and target planet
-    transit_single = find_transits(integration_steps)
+    #transit_single = find_transits(integration_steps)
     # reset system positions
     system.resetPropogation()
     # compute transits for the system with a perturbing body
     system.addBody(p2)
     transit_full = find_transits(integration_steps)
 
-    # fetch the number of transits in our data
-    idxs = min(len(transit_single), len(transit_full))
-    transit_full, transit_single = transit_full[:idxs], transit_single[:idxs]
-
-
-
+    # indexes for the transsits
+    idxs = len(transit_full)
+    # recover TTV using least squares regression
     A = np.vstack([np.ones(idxs), range(idxs)]).T
     c, m = np.linalg.lstsq(A, transit_full, rcond=-1)[0]
 
-    plt.plot(range(idxs), (transit_full-m*np.array(range(idxs))-c))
+    x_times = range(idxs)
+
+    # find the residuals from the least squares
+    residuals = (transit_full - m*np.array(range(idxs))-c)
+
+    # show the found TTV
+    plt.plot(range(idxs), residuals)
     plt.show()
 
-
-    # create an array of orbits to plot against
-    orbit_num = np.arange(len(transit_full))
-
-    # find the difference in single vs perturbed transit
-    perturbed = transit_single - transit_full
-
     # create a model to fit the data against
-    def linefit(x, a, b):
+    '''def linefit(x, a, b):
         return a * x + b
     # fit a straight line to the perturbed value
     popt, pcov = curve_fit(linefit, orbit_num, perturbed)
     # and compute the residuals
     residuals = perturbed - linefit(orbit_num, *popt)
     plt.plot(orbit_num, residuals)
-    plt.show()
+    plt.show()'''
 
     # compute the average sampling rate
-    duration = max(transit_full[-1], transit_single[-1]) - min(transit_full[0], transit_single[0])
+    duration = transit_full[-1] - transit_full[0]
     sampling_rate = len(transit_full) / duration
 
     # compute the fourier transform of the residuals
@@ -159,13 +155,13 @@ def main():
     plt.show()
 
 
-
+    # attempt to fit to two sine functions
     def sinfunc(x, a, b, c, d, e, f, g):
         return a * np.sin(b*(x - c)) + d * np.sin(e*(x - f)) + g
     params = (1, 0.5, 0.5, 1, 0.5, 0.5, 1)
-    popt, pcov = curve_fit(sinfunc, orbit_num, residuals, p0=params)
-    plt.scatter(orbit_num,residuals)
-    plt.plot(orbit_num, sinfunc(orbit_num, *popt), 'r-',label='Fitted function')
+    popt, pcov = curve_fit(sinfunc, x_times, residuals, p0=params)
+    plt.scatter(x_times,residuals)
+    plt.plot(x_times, sinfunc(x_times, *popt), 'r-',label='Fitted function')
     plt.legend()
     plt.show()
 
