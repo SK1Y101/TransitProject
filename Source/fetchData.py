c0ab485f416288a440a4eed3de960deeb9be8083
sk1y101 <skiy@outlook.com>
2022-03-27 16:28:24 +0100

    a

--- Source/fetchData.py
+++ Source/fetchData.py
@@ -16,6 +16,7 @@ ws.fetchETDData("http://var2.astro.cz/ETD/index.php?lang=en", sourceName="ETD")
 # Exoclock (Lightcurves & system info)
 # Access system information
 ws.saveDataDict("https://www.exoclock.space/database/planets_json", "/raw_data/exoClockEphemerides.csv")
+
 # fetch midtransit data
 ws.fetchExoClockData("https://www.exoclock.space/database/observations", sourceName="ExoClock observation")
 ws.fetchExoClockData("https://www.exoclock.space/database/literature", sourceName="ExoClock literature")
