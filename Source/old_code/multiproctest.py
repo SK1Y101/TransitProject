8e1b3e3cc6a9cfcf33a7d062bab2cbabdf5d56a6
sk1y101 <skiy@outlook.com>
2022-04-05 23:48:18 +0100

    output of bar that isn't bad

--- Source/old_code/multiproctest.py
+++ Source/old_code/multiproctest.py
@@ -6,8 +6,8 @@ import numpy as np
 
 def testfunc(sim, tstep, transits=1000):
     workerId = multiprocessing.current_process()._identity[0]
-    for x in trange(10+workerId, leave=None, position=workerId, smoothing=1):
-        wait(.25)
+    for x in trange(10+workerId, leave=None, position=workerId):
+        wait(0.1)
     return workerId
 
 sim = np.random.rand(100, 10)
@@ -16,13 +16,13 @@ itterable = [(sim[x], 100, 1000) for x in range(len(sim))]
 maxjobs = len(itterable)
 _complete=0
 
-with tqdm(total = maxjobs, position=0, leave=False, desc="Multiprocessing pool", smoothing=0) as bar:
+with tqdm(total = maxjobs, leave=False, desc="Multiprocessing pool", smoothing=-.3) as bar:
     with Pool(8) as p:
         r=p.starmap_async(testfunc, itterable, chunksize=1)
         while not r.ready():
             completed = maxjobs-r._number_left
             bar.update((completed)-_complete)
             _complete=completed
-            wait(0.1)
-        bar.update(1)
-        print(r.get())
+    bar.n = bar.total
+    bar.refresh()
+    print(r.get())
