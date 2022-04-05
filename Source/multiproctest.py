cb13ac650dad62689dfec8770f0b6191221e2999
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2022-04-05 20:30:37 +0100

    testing how multiprocessing works

---
+++ Source/multiproctest.py
@@ -0,0 +1,17 @@
+from multiprocessing import Pool
+from time import sleep as wait
+import numpy as np
+
+def testfunc(sim, tstep, transits=1000, i=0):
+    return i
+
+sim = np.random.rand(int(5E4), 10)
+
+itterable = [(sim[x], 100, 1000, x) for x in range(len(sim))]
+
+with Pool() as p:
+    r=p.starmap_async(testfunc, itterable)
+    while not r.ready():
+        print(r._number_left)
+        wait(0.5)
+    print(r.get())
