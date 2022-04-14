(hopefully) fix innacurate TTV calculation

Source/TransitProject/Simulation.py
@@ -55,23 +55,29 @@ def _fetchSystem_(target, params=["mass", "sma", "per", "ecc", "inc", "arg"]):
     ''' Fetch the system information for a given target planet.
         target: The name of the planet to fetch the information for.
         params: List of parameters to fetch from the stored databases. '''
+    # if we didn't include mass or sma, include them (minimum needed for simulation)
+    params = ["mass"]+params if "mass" not in params else params
+    params = ["sma"]+params if ("sma" not in params) or ("per" not in params) else params
     # fetch the central body name
     star = target[:-1]
     # add errors to the paramaters, and map to archive names
-    params = ",".join([",".join([param,param+"_e1",param+"_e2"]) for param in params]).split(",")
-    aparams = mapToArchive(params)
+    def allParams(params):
+        params = ",".join([",".join([param,param+"_e1",param+"_e2"]) for param in params]).split(",")
+        aparams = mapToArchive(params)
+        return params, aparams
+    param, aparam = allParams(params)
     # load the dataframe
     archive = loadDataFrame("/raw_data/pscomppars.csv")
     # fetch all planets with the required hostname
-    data = archive.loc[archive["hostname"].str.replace(" ", "") == star].reset_index()
+    data = archive[archive.hostname.str.replace(" ", "").isin([star, star.lower(), star.upper(), star.capitalize()])].reset_index()
     # fetch the target planet index
-    targetIdx = np.where(data["pl_name"].str.replace(" ","") == target)[0][0]
+    targetIdx = np.where(data.pl_name.str.replace(" ", "").isin([target, target.lower(), target.upper(), target.capitalize()]))[0][0]
     # ensure it is the first value in the dataframe
     if targetIdx != 0:
         # move the specified row to the top of the dataFrame
         data = moveRowToTop(data, targetIdx)
     # construct a new dataframe of only required parameters
-    systemData = data.loc[:, ["pl_name"]+aparams]
+    systemData = data.loc[:, ["pl_name"]+aparam]
     # convert mass to solar mass
     if "pl_bmassj" in systemData.columns:
         systemData.loc[:, ["pl_bmassj", "pl_bmassjerr1", "pl_bmassjerr2"]] *= 9.55E-4
@@ -81,6 +87,9 @@ def _fetchSystem_(target, params=["mass", "sma", "per", "ecc", "inc", "arg"]):
         systemData = convert(systemData, degcols[1:], np.radians)
         # because inclination is perpendicular to earth, we need to deal with this slightly differently
         systemData = convert(systemData, degcols[0], lambda x: np.radians(90-x))
+    # convert period to years
+    if "pl_orbper" in systemData.columns:
+        systemData = convert(systemData, mapToArchive(["per", "per_e1", "per_e2"]), lambda x:x/365.25)
     # populate a new list with the stars data
     d1 = data.iloc[0]
     starIdx, starData = len(systemData.index), [d1["hostname"], d1["st_mass"], d1["st_masserr1"], d1["st_masserr2"]]
@@ -89,7 +98,7 @@ def _fetchSystem_(target, params=["mass", "sma", "per", "ecc", "inc", "arg"]):
     # and shift it to the top
     systemData = moveRowToTop(systemData, starIdx)
     # rename all the columns to be more usefull
-    systemData.columns = ["name"]+params
+    systemData.columns = ["name"]+param
     # return the data
     return systemData
 
@@ -194,7 +203,7 @@ def valFromParam(value, array, param):
     # otherwise, return the part of the array that corresponds
     return noneIfInf(array[param.index(value)])
 
-def _simulateTT_(sim, timestep, transits=1000, i=1, prec=1E-7):
+def _simulateTT_(sim, timestep, transits=1000, i=1, prec=1/31557600.0):
     ''' Simulate a system and find the transit times.
         sim: A rebound Simulation object to integrate. (particle 1 is assumed to be the target)
         timestep: The timestep to integrate over.
@@ -239,7 +248,7 @@ def _simulateTT_(sim, timestep, transits=1000, i=1, prec=1E-7):
             sim.integrate(sim.t+timestep)
     # return the found transit times
     # because we are working with G=1, a=1 [AU], then t [year]=2pi
-    return tarray / (2*np.pi)
+    return tarray
 
 def _arrayToSim_(thisSim, params):
     ''' Generates a list of simulations from an array of simulation parameters.
@@ -249,6 +258,8 @@ def _arrayToSim_(thisSim, params):
     objs = len(thisSim) / len(params)
     # create the simulation
     sim = rebound.Simulation()
+    # set the units for the simulation
+    sim.units = ('yr', 'AU', 'Msun')
     # iterate on each object
     for obj in np.split(thisSim, objs):
         # fetch this objects paramaters
@@ -256,41 +267,50 @@ def _arrayToSim_(thisSim, params):
         # if we have a semimajor axis
         if sma:
             sim.add(m=mass, a=sma, e=ecc, omega=arg, inc=inc)
-        else:
+        elif per:
+            # because of our units of G=1, a=1 [AU], then t [year]=2pi
             sim.add(m=mass, P=per, e=ecc, omega=arg, inc=inc)
+        else:
+            # the star has no interesting properties beyond mass
+            sim.add(m=mass)
     # move to the centre of mass
     sim.move_to_com()
     # and return the simulation
     return sim
 
-def _simulateTransitTimes_(simArray, params, transits=1000):
+def _simulateTransitTimes_(simArray, params, transits=1000, prec=1/31557600.0):
     ''' Locate all transit times for a given simulation setup.
         simArray: The array of simulation parameters.
         params: The list of parameter names. used to map the array to usable values.
-        transits: The number of transits to locate. '''
+        transits: The number of transits to locate.
+        prec: The precision, defaults to 1 second. '''
     # fetch this simulation
     sim = _arrayToSim_(simArray, params)
     # fetch the smallest orbital period in the system
     p_orb = min([x.P for x in sim.particles[1:]])
-    # ensure our timestep is either hourly, or one tenth the smallest orbital period
-    timestep = min(p_orb*0.1, 1 / (24*365))
+    # ensure our timestep is resonable: Take the smallest orbital period:
+    # if it is very small, step in units of 0.1 periods.
+    # if very large, step in units of 0.001 periods.
+    # if niehter, step hourly
+    timestep = max(p_orb*0.0001, min(p_orb*0.1, 1 / (24*365)))
     # fetch the position from the worker ID
     pos = curProc()._identity[0] if curProc().name != "MainProcess" else None
     # simulate for the chosen number of transits
-    TT = _simulateTT_(sim, timestep, transits, pos)
+    TT = _simulateTT_(sim, timestep, transits, pos, prec)
     # and return the transit times
     return np.array(TT)
 
-def fetchTT(simArray, params, transits=1000, workers=None, tqdmLeave=True, returnAll=False):
+def fetchTT(simArray, params, transits=1000, prec=1/31557600.0, workers=None, tqdmLeave=True, returnAll=False):
     ''' Create a multiprocessing job to simulate all possible configurations of a planetary system.
         returns the transit times, the upper, and the lower error bounds.
         simArray: The 2D array of all possible simulation parameters.
         params: The list of parameter names. used to map the array to usable values.
         transits: The number of transits to simulate for.
+        prec: The precision, defaults to 1 second.
         workers: The number of worker processes to use for the simulation (defaults to cpucount / 2).
         tqdmLeave: Whether to leave the overarchive tqdm bar when completed.
         returnAll: Whether to return the entire output, or just the average/minimum/maximum.'''
-    inputs = toItterableInput(simArray, params, transits, keep=(1,))
+    inputs = toItterableInput(simArray, params, transits, prec, keep=(1,))
     # run the multiprocessing job
     TT = parallelJob(_simulateTransitTimes_, inputs, outType=np.array, workers=workers, tqdmLeave=tqdmLeave)
     # if we want to return everything, do that

Source/processing.py
@@ -3,6 +3,7 @@ import matplotlib.pylab as plt
 import scipy.optimize
 import pandas as pd
 import numpy as np
+import argparse
 
 # my modules
 import TransitProject as tp
@@ -57,7 +58,7 @@ def dateToYearFloat(date):
     return pd.to_datetime(date).value / nanoSecToDay
 
 def fetchFit(x, y, err=None, f=lambda x,a,b:a*x+b):
-    pars, corr = scipy.optimize.curve_fit(f, x, y, sigma=err)
+    pars, corr = scipy.optimize.curve_fit(f, x, y, sigma=err, maxfev=5000)
     return lambda x:f(x, *pars)
 
 def plotMidtransits(df):
@@ -66,12 +67,10 @@ def plotMidtransits(df):
     # if there are any
     if sources:
         df = df.loc[df["source"]!="ETD"]
-        #df = df.sort_values(by="date", ascending=True)
+        df = df.sort_values(by="date", ascending=True)
 
-        def f(x, a, b):
-            c=0.013 * 24 * 60
-            d=428
-            return c*np.sin(x*(2*np.pi/d)+a) + b
+        def f(x, a, b, c, d):
+            return a*np.sin(x*(2*np.pi/b)+c) + d
 
         fig = plt.figure()
         ax = fig.add_gridspec(2, hspace=0).subplots(sharex=True)
@@ -104,7 +103,12 @@ def plotMidtransits(df):
             axs.legend()
         plt.show()
 
-df = fetchMidTransitTimes("Corot-11b")
-#df = fetchMidTransitTimes("CoRoT-2b")
-df = fetchMidTransitTimes("HAT-P-13b")
+# fetch the planet from command line input
+parser = argparse.ArgumentParser(description="Simulate transit timing variations.")
+parser.add_argument("--planet", help="The name of the planet to simulate TTV for.")
+args = parser.parse_args()
+if not args.planet:
+    raise Exception("No planet provided")
+
+df = fetchMidTransitTimes(args.planet)
 plotMidtransits(df)

Source/simulationPipeline.py
@@ -1,9 +1,9 @@
 # python modules
 from multiprocessing import current_process as curProc
+import rebound, argparse, scipy.optimize
 import matplotlib.pylab as plt
 from tqdm import tqdm, trange
 import numpy as np
-import rebound
 
 # my modules
 import TransitProject.Simulation as ts
@@ -18,15 +18,31 @@ Goals: Collect system information from a local database.
 def computeTTV(transitTimes):
     ''' Compute the transit timing variation from an array of transit times using linear regression.
         transitTimes: the array of transit times. '''
+    # number of transits
     N = len(transitTimes)
-    # construct an empty 2d array for regression
-    A = np.vstack([np.ones(N), range(N)]).T
-    # perform regression to get coefficients of linear fit
-    c, m = np.linalg.lstsq(A, transitTimes, rcond=-1)[0]
+    # linear function
+    f = lambda x,a,b: a*x+b
+    # decompose the inputs to x,y
+    x, y = np.arange(N), transitTimes
+    # fit to transit times
+    pars, corr = scipy.optimize.curve_fit(f, x, y, maxfev=5000)
     # subtract from the transit times to get the variation
-    TTV = transitTimes - m*np.arange(N) - c
+    TTV = transitTimes - f(x, *pars)
     # and return
     return TTV
+
+# fetch the planet from command line input
+parser = argparse.ArgumentParser(description="Simulate transit timing variations.")
+parser.add_argument("--planet", help="The name of the planet to simulate TTV for.")
+parser.add_argument("--years", help="The number of years to simulate TTV for.\nDefaults to 4.")
+parser.add_argument("--workers", help="The number of workers to use for multiprocessing.\nDefaults to half the available CPU cores")
+parser.add_argument("--precision", help="The precision (in seconds) of the simulation.\nDefaults to 1 second.")
+args = parser.parse_args()
+years = float(args.years) if args.years else 4
+workers = int(args.workers) if args.workers else None
+precision = float(args.precision if args.precision else 1) / 31557600
+if not args.planet:
+    raise Exception("No planet provided")
 
 # define the simulation parameters
 ''' choice of mass, period or semimajor axis, eccentiricty, inclination, argument of periapsis '''
@@ -34,35 +50,33 @@ def computeTTV(transitTimes):
 params = ["mass", "sma", "ecc", "inc", "arg"]
 
 # fetch the parameters for the system
-df = ts.fetchParams("HAT-P-13 b")
+df = ts.fetchParams(args.planet)
 
 # compute the number of transits needed
-times = 2008, 2022
-N=int(np.ceil((times[1]-times[0])*365.25/df.iloc[1]["per"]))
-
-# manually removing errors
-for x in params:
-    for y in [1,2]:
-        df[x+"_e{}".format(y)] = 0
+N, a = int(np.ceil(years/df.iloc[1]["per"])), 1
+# ensure the outermost planet transits at least ten times (to bring out the TTV signal)
+N = max(N, int(np.ceil(N * 10 * max(df.per[df.per.notnull()]) / years)))
 
 # construct the aray of simulation parameters
 simArray = ts.constructSimArray(df, params)
 
 # fetch the transit times from simulation
-TT = ts.fetchTT(simArray, params, N, returnAll=True)
+TT = ts.fetchTT(simArray, params, N, prec=precision, returnAll=True, workers=workers)
 
-# compute the TTV for all values
-TTV = [computeTTV(tt) for tt in TT]
+# compute the TTV for all values, convert to minutes
+TTV = [computeTTV(tt) * 365.25 * 1440 for tt in TT]
 # and the error bounds (upper and lower)
 TTVa, TTVl, TTVu = tp.avMinMax(TTV, 0)
 
 # error area
+plt.plot(TTVl, color="black", label="TTV Range (Lower estimate)")
+plt.plot(TTVu, color="black", label="TTV Range (Upper estimate)")
 #plt.fill_between(range(N), TTVl, TTVu, color="gray", label="TTV error")
 # main value
-for TTVb in TTV:
-    plt.plot(TTVb[:300], color="black", label="Predicted TTV")
+plt.plot(TTVa, color="black", label="Predicted TTV")
 #labels
+plt.xlim([0, int(np.ceil(years/df.iloc[1]["per"]))])
 plt.xlabel("Epoch")
-plt.ylabel("Time [Days]")
+plt.ylabel("Time [Minutes]")
 plt.legend()
 plt.show()