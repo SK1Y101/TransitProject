a596683a569e6c5d7c141b4013695d189d132669
sk1y101 <skiy@outlook.com>
2022-05-04 16:06:33 +0100

    test process to fit midtransits to gaussian TESS data

--- Source/TransitProject/tesslc.py
+++ Source/TransitProject/tesslc.py
@@ -94,10 +94,10 @@ def _lightcurves_(target, loc="/raw_data/tess_data/", stale_time=7):
                     units = unit
         # create dataframe from the data
         df = pd.DataFrame()
-        df["Time"], df["Flux"], df["Flux_error"] = times, fluxes, ferrs
+        df["time"], df["flux"], df["flux_e"] = times, fluxes, ferrs
         # convert our times from bjd - 2457000 to bjd
         bjdoffset = findFloats(unit[0])[0].split(" ")[1]
-        df["Time"] += int(bjdoffset)
+        df["time"] += int(bjdoffset)
         # save the dataframe
         saveDataFrame(df, lcfname)
         # and return it too
@@ -106,9 +106,98 @@ def _lightcurves_(target, loc="/raw_data/tess_data/", stale_time=7):
     raise Exception("No LightCurve Data for {}".format(target))
 
 def tessMidTransits(target, loc, stale_time):
+    # lowercase the target name
+    target = target.lower()
     # ensure the tess lightcurve folder exists
     makeFolder(loc)
     # fetch the lightcurve data
     df = _lightcurves_(target, loc, stale_time)
+    print("LC Found")
+    # decompose into time, flux, and error
+    t, f, ferr = np.array(df["time"]), np.array(df["flux"]), np.array(df["flux_e"])
+    # Period and t0 from Anderson et al. (201X):
+    P,t0 =  4.7423729 ,  2457376.68539 - 2457000
+    # Get phases --- identify out-of-transit (oot) times by phasing the data
+    # and selecting all points at absolute phases larger than 0.02:
+    phases = juliet.utils.get_phases(t, P, t0)
+    idx_oot = np.where(np.abs(phases)>0.02)[0]
+    # Save the out-of-transit data into dictionaries so we can feed them to juliet:
+    times, fluxes, fluxes_error = {},{},{}
+    times['TESS'], fluxes['TESS'], fluxes_error['TESS'] = t[idx_oot], f[idx_oot], ferr[idx_oot]
+    # First define the priors:
+    priors = {}
+
+    # Same priors as for the transit-only fit, but we now add the GP priors:
+    params = ['P_p1','t0_p1','r1_p1','r2_p1','q1_TESS','q2_TESS','ecc_p1','omega_p1',\
+              'rho', 'mdilution_TESS', 'mflux_TESS', 'sigma_w_TESS', \
+              'GP_sigma_TESS', 'GP_rho_TESS']
+
+    dists = ['normal','normal','uniform','uniform','uniform','uniform','fixed','fixed',\
+             'loguniform', 'fixed', 'normal', 'loguniform', \
+             'loguniform', 'loguniform']
+
+    hyperps = [[4.7,0.1], [1329.9,0.1], [0.,1], [0.,1.], [0., 1.], [0., 1.], 0.0, 90.,\
+               [100., 10000.], 1.0, [0.,0.1], [0.1, 1000.], \
+               [1e-6, 1e6], [1e-3, 1e3]]
+
+    # Populate the priors dictionary:
+    for param, dist, hyperp in zip(params, dists, hyperps):
+        priors[param] = {}
+        priors[param]['distribution'], priors[param]['hyperparameters'] = dist, hyperp
+
+    print("Params set")
+
+    times['TESS'], fluxes['TESS'], fluxes_error['TESS'] = t,f,ferr
+    dataset = juliet.load(priors=priors, t_lc = times, y_lc = fluxes, \
+                          yerr_lc = fluxes_error, GP_regressors_lc = times, \
+                          out_folder = '{}_transitGP'.format(target.replace(" ","_")), verbose = True)
+    print("Dataset saved")
+
+    results = dataset.fit()
+    # Extract full model:
+    transit_plus_GP_model = results.lc.evaluate('TESS')
+
+    # Deterministic part of the model (in our case transit divided by mflux):
+    transit_model = results.lc.model['TESS']['deterministic']
+
+    # GP part of the model:
+    gp_model = results.lc.model['TESS']['GP']
+
+    # Now plot. First preambles:
+    fig = plt.figure(figsize=(12,4))
+    gs = gridspec.GridSpec(1, 2, width_ratios=[2,1])
+    ax1 = plt.subplot(gs[0])
+
+    # Plot data
+    ax1.errorbar(dataset.times_lc['TESS'], dataset.data_lc['TESS'], \
+                 yerr = dataset.errors_lc['TESS'], fmt = '.', alpha = 0.1)
+
+    # Plot the (full, transit + GP) model:
+    ax1.plot(dataset.times_lc['TESS'], transit_plus_GP_model, color='black',zorder=10)
+
+    ax1.set_xlim([1328,1350])
+    ax1.set_ylim([0.96,1.04])
+    ax1.set_xlabel('Time (BJD - 2457000)')
+    ax1.set_ylabel('Relative flux')
+
+    ax2 = plt.subplot(gs[1])
+
+    # Now plot phase-folded lightcurve but with the GP part removed:
+    ax2.errorbar(phases, dataset.data_lc['TESS'] - gp_model, \
+                 yerr = dataset.errors_lc['TESS'], fmt = '.', alpha = 0.3)
+
+    # Plot transit-only (divided by mflux) model:
+    idx = np.argsort(phases)
+    ax2.plot(phases[idx],transit_model[idx], color='black',zorder=10)
+    ax2.yaxis.set_major_formatter(plt.NullFormatter())
+    ax2.set_xlabel('Phases')
+    ax2.set_xlim([-0.03,0.03])
+    ax2.set_ylim([0.96,1.04])
     # fit transits to the lightcurve data using juliet
     #midTransitTimes = fitTransits(df["Times"])
+    #plt.errorbar(t, f, yerr=ferr, fmt='.', alpha=0.2)
+    #plt.xlabel('Time (BJD)')
+    #plt.ylabel('Relative flux')
+    #plt.xlim((2457000 + 1325, 2457000+1355))
+    #plt.ylim((0.94, 1.06))
+    plt.show()
