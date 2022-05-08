1a56c8834836621c867dfa2ab05a3753cfff27e2
sk1y101 <skiy@outlook.com>
2022-05-08 14:49:02 +0100

    hats-46 midtransit times

--- Source/plotTessFindings.py
+++ Source/plotTessFindings.py
@@ -3,6 +3,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Plot simulations and models")
 parser.add_argument("--star", help="The name of the star to search the lightcurve of.", required=True)
+parser.add_argument("--saveTTV", help="Whether the results of the TTV should be saved after graphing.", action="store_true")
 args = parser.parse_args()
 
+# if we are saving the TTV
+if args.saveTTV:
+    # Run the fetch & save code
+    ws.fetchTESSTTV(target=args.star)
+# otherwise, just plot
 ws.plotTESS(target=args.star)
